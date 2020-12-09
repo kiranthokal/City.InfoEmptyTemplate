ccf81faccbff0abcb1442583a12269ce028a5af0
Kiran Thokal <kiranthokal@ghgghjghjb.com>
2020-12-09 18:03:51 +0530

    Add project files.

---
+++ City.InfoEmptyTemplate/Controllers/CitiesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace City.InfoEmptyTemplate.Controllers
+{
+    [ApiController]   
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>
+            {
+                new {id = 1, Name = "Pune"},
+                new {id = 2, Name = "Mumbai"}
+            });
+        }
+
+        [HttpGet("api/cities/id")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(new { id = 1, Name = "Pune" });
+        }
+    }
+}
